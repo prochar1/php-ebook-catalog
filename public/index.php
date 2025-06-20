7428d05342f584c10050672794bdfbea0e40fadc
ProchaR1 <prochar1@gmail.com>
2025-06-20 14:11:54 +0200

    Refactor routing setup in index.php to improve organization and clarity

--- public/index.php
+++ public/index.php
@@ -1,4 +1,24 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$router = new App\Core\Router();
+use App\Core\Router;
+use App\Controllers\BookController;
+
+$router = new Router();
+
+$router->get('/', [BookController::class, 'index']);
+$router->get('/books', [BookController::class, 'index']);
+$router->get('/books/{id}', [BookController::class, 'show']);
+
+$router->get('/admin', [BookController::class, 'admin']);
+$router->post('/admin/import', [BookController::class, 'import']);
+
+$router->get('/books/create', [BookController::class, 'create']);
+$router->post('/books/create', [BookController::class, 'create']);
+
+$router->get('/books/{id}/edit', [BookController::class, 'update']);
+$router->post('/books/{id}/edit', [BookController::class, 'update']);
+
+$router->post('/books/{id}/delete', [BookController::class, 'delete']);
+
+$router->dispatch();
